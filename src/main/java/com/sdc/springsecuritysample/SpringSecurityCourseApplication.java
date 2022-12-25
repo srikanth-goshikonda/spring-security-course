6d41033695771c7ad24ade5c19d6cbe2a0b64a86
Srikanth Goshikonda <srikanthgoshikonda@gmail.com>
2022-12-25 12:03:58 +0530

    Initial Setup

---
+++ src/main/java/com/sdc/springsecuritysample/SpringSecurityCourseApplication.java
@@ -0,0 +1,13 @@
+package com.sdc.springsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdc/springsecuritysample/SpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdc.springsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
